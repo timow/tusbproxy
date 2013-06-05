94da491ba5a745a1968249a53856a0980723b429
Timo Warns <timow+github@DiningPhilosopher.DE>
2013-06-05 20:56:57 +0200

    added USB setup packet definition

--- src/client/usb_20.py
+++ src/client/usb_20.py
@@ -28,3 +28,41 @@ DESCRIPTOR_TYPE = {
         'OTHER_SPEED_CONFIGURATION'         : 7,
         'INTERFACE_POWER'                   : 8
         }
+
+# Section 9.3. Setup Data
+class Setup(Packet):
+    name = 'USB Setup Packet'
+
+    DIR_HOST_TO_DEVICE = 0
+    DIR_DEVICE_TO_HOST = 1
+
+    TYPE_STANDARD = 0
+    TYPE_CLASS    = 1
+    TYPE_VENDOR   = 2
+    TYPE_RESERVED = 3
+    
+    RCPT_DEVICE      = 0
+    RCPT_INTERFACE   = 1
+    RCPT_ENDPOINT    = 2
+    RCPT_OTHER       = 3
+
+
+    fields_desc = [
+            BitEnumField('data_xfer_direction', 0, 1, {
+                DIR_HOST_TO_DEVICE : 'Host-to-device',
+                DIR_DEVICE_TO_HOST : 'Device-to-host'}),
+            BitEnumField('type', 0, 2, {
+                TYPE_STANDARD : 'Standard',
+                TYPE_CLASS    : 'Class',
+                TYPE_VENDOR   : 'Vendor',
+                TYPE_RESERVED : 'Reserved'}),
+            BitEnumField('recipient', 0, 5, {
+                RCPT_DEVICE    : 'Device',
+                RCPT_INTERFACE : 'Interface',
+                RCPT_ENDPOINT  : 'Endpoint',
+                RCPT_OTHER     : 'Other'}),
+            ByteEnumField('request', 0, REQUEST_CODE),
+            LEShortField('value', 0),
+            LEShortField('index', 0),
+            LEShortField('length', 0),
+            ]
